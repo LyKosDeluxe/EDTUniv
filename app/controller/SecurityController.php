b86a20348eda03986a49ac90f5667dfd20a70198
FireFull45 <firefull2945@gmail.com>
2022-10-05 16:42:32 +0200

    modification bouton header

--- app/controller/SecurityController.php
+++ app/controller/SecurityController.php
@@ -21,11 +21,13 @@ class SecurityController extends Controller
      */
     public function main(): String
     {
-        if(Auth::isAuth()) {
+        if (Auth::isAuth()) {
             header('Location: /edt');
             return '';
         }
-        return $this->render('security.html.twig',[
+        return $this->render(
+            'security.html.twig',
+            [
                 'android' => OperatingSystem::isAndroid(),
                 'iphone' => OperatingSystem::isApple(),
                 'pwa' => OperatingSystem::isPwa()
@@ -36,27 +38,25 @@ class SecurityController extends Controller
     public function login()
     {
         try {
-            if((isset($_POST['tdGrp']) && is_numeric($_POST['tdGrp'])) && (isset($_POST['tpGrp']) && is_numeric($_POST['tpGrp'])) && isset($_POST['username']) && !empty($_POST['username']))
-            {
-                if(Auth::isAuth()) throw new AlreadyAuthException();
+            if ((isset($_POST['tdGrp']) && is_numeric($_POST['tdGrp'])) && (isset($_POST['tpGrp']) && is_numeric($_POST['tpGrp'])) && isset($_POST['username']) && !empty($_POST['username'])) {
+                if (Auth::isAuth()) throw new AlreadyAuthException();
 
                 $tp = intval(htmlspecialchars($_POST['tpGrp']));
                 $td = intval(htmlspecialchars($_POST['tdGrp']));
-                if($tp == 0 OR $td == 0) throw new InputAuthException();
-                Auth::login(htmlspecialchars($_POST['username']),["tp", $tp], ["td", $td]);
+                if ($tp == 0 or $td == 0) throw new InputAuthException();
+                Auth::login(htmlspecialchars($_POST['username']), ["tp", $tp], ["td", $td]);
                 header('Location: /edt');
             }
-        } catch (AlreadyAuthException|InputAuthException $e)
-        {
+        } catch (AlreadyAuthException | InputAuthException $e) {
             header('Location: /');
         }
     }
-    public function logout() : void {
+    public function logout(): void
+    {
         try {
-            if(Auth::isAuth()) Auth::logout();
+            if (Auth::isAuth()) Auth::logout();
             else throw new NotAuthException();
-        } catch(NotAuthException $e)
-        {
+        } catch (NotAuthException $e) {
         }
         header('Location: /');
     }
